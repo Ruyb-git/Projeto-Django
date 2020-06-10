148d74fd0071640c1eaa33512406b6b24ad2045a
RuybProgramming <rubenilsondesousa@gmail.com>
2020-06-10 14:53:23 -0300

    adicionando secret key via variável de ambiente

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -22,7 +22,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q37vaci*j=sokz04h*nxsxd1(%)%yjk$5ltg2pixirh+f91pbb'
+SECRET_KEY = os.getenv('SECRET_KEY','123')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
